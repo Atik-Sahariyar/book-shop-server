8e7166e187c899af4ae01ae62da7d200ebc97113
Atik-Sahariyar <atiksahariyar88@gmail.com>
2024-11-23 23:20:09 +0600

    successfully connected to db

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
-const app = express()
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
+
+const app: Application = express();
+
 
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
+app.use(bodyParser.json());
+// app.use("/api/products", productRoutes);
+// app.use("/api/orders", orderRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app"
-import config from "./app/config";
-
-
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
-    try{
-        await mongoose.connect(config.database_url as string);
+  try {
+    console.log("connecting to database ...")
+    await mongoose.connect(config.database_url as string, { dbName: config.db_name });
 
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-        })
-    } catch (error){
-        console.log(error)
-    }
-
+    app.listen(config.port, () => {
+      console.log(`Book shop server is runing on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  main();
-
+main();
